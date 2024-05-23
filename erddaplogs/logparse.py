03d845a5a8c1538cde1c40ec5787bcbeb44d268d
Tuomo S <tuosaa@gmail.com>
2024-05-23 14:03:09 -0500

    Second try at fixing issue #42. Have sensible defaults for the logfile names.

--- erddaplogs/logparse.py
+++ erddaplogs/logparse.py
@@ -325,7 +325,7 @@ class ErddapLogParser:
 
     def load_apache_logs(self, apache_logs_dir: str, wildcard_fname: str):
         """Parse apache logs."""
-        df_apache = _load_apache_logs(apache_logs_dir, wildcard_fname)
+        df_apache = _load_apache_logs(apache_logs_dir, wildcard_fname="*access.log*")
         if self.verbose:
             print(f"loaded {len(df_apache)} log lines from {apache_logs_dir}")
         df_combi = pl.concat(
@@ -341,7 +341,7 @@ class ErddapLogParser:
 
     def load_nginx_logs(self, nginx_logs_dir: str, wildcard_fname: str):
         """Parse nginx logs."""
-        df_nginx = _load_nginx_logs(nginx_logs_dir, wildcard_fname)
+        df_nginx = _load_nginx_logs(nginx_logs_dir, wildcard_fname="*access.log*")
         if self.verbose:
             print(f"loaded {len(df_nginx)} log lines from {nginx_logs_dir}")
         df_combi = pl.concat(
